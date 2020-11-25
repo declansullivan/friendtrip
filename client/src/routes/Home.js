3126001124e1298866cc5c99860e61529f0ce0e3
Fangzheng <fal044@ucsd.edu>
2020-11-25 11:42:01 -0800

    add logout button to the homepage

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class Home extends Component {
     constructor(props) {
         super(props);
     }
 
-    signIn = event => {
-
+    logOut = () => {
+        this.props.history.push('/');
+        localStorage.clear();
     }
 
     render() {
         return (
             <div class="centerdiv">
                 <h1> HOME PAGE </h1>
+                <Button variant="primary" onClick={this.logOut}>Logout</Button>
             </div>
         )
     }
