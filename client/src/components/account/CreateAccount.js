3d439e76f11adae16ff12d2d508613f773e65331
StoneAlchemist <StoneAlchemist@github.com>
2020-11-30 15:34:02 -0800

    Signup now redirects to home

--- client/src/components/account/CreateAccount.js
+++ client/src/components/account/CreateAccount.js
@@ -39,6 +39,7 @@ class CreateAccount extends Component {
         }
         else {
           alert("Account successfully created!");
+          localStorage.setItem("id", data.email.replace('.', ''));
           this.props.history.push("/");
         }
       });
