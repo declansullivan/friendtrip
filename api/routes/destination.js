8bc55feb6d2941af7ba22dd475faa33c0bd1a764
Declan Sullivan <declan@declansullivan.com>
2020-11-29 17:32:53 -0800

    Refactoring methods in backend

--- api/routes/destination.js
+++ api/routes/destination.js
@@ -2,15 +2,15 @@ var express = require('express');
 var Destination = require('../db/models/destination');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.send("Destination API.");
-});
-
-router.put('/addDestination', function(req, res, next) {
+router.post('/getDestination', function(req, res, next) {
     res.send("Not Implemented!");
 });
 
-router.get('/viewDestination', function(req, res, next) {
+router.post('/getDestinations', function(req, res, next) {
+    res.send("Not Implemented!");
+});
+
+router.put('/addDestination', function(req, res, next) {
     res.send("Not Implemented!");
 });
 
